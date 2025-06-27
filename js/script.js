7a7d0429654807498b4de878eaa02329786e8d56
SHCK <shamil.shck@gmail.com>
2025-06-27 20:08:23 +0530

    working on it , need to complete js for guesssubmit and gues sfield

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let randomNum = Math.floor(Math.random()*100)+1;
+
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const loworHi = document.querySelector(".loworHi");
+
+
+
