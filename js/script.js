a21bb572b138302e6467395343acaa447eaa1831
SHCK <shamil.shck@gmail.com>
2025-06-27 20:37:26 +0530

    feat:guess checking added and script linked html doc

--- js/script.js
+++ js/script.js
@@ -8,6 +8,52 @@ const loworHi = document.querySelector(".loworHi");
 const guessSubmit = document.querySelector(".guessSubmit");
 const guessField = document.querySelector(".guessfield");
 
+let guessCount=1;
+let resetButton;
 
+
+function checkGuess(){
+    const userGuess = Number(guessField.value);
+    if (guessCount === 1){
+        guesses.textContent = "previous guesses: ";
+    }
+    guesses.textContent = `${guesses.textContent} ${userGuess}`;
+
+    if(userGuess === randomNum){
+
+        lastResult.textContent="Congratulations! You got it right!";
+        lastResult.style.backgroundColor ="green";
+        loworHi = "";
+        setGameOver();
+
+    }
+    else if (guessCount === 10) 
+        {
+
+            lastResult.textContent = "!!!GAME OVER!!!";
+            lowOrHi.textContent = "";
+            setGameOver();
+
+        } 
+    else {
+
+        lastResult.textContent = "Wrong!";
+        lastResult.style.backgroundColor = "red";
+
+        if (userGuess < randomNumber) {
+
+            lowOrHi.textContent = "Last guess was too low!";
+        
+        } 
+        else if (userGuess > randomNumber) {
+            
+            lowOrHi.textContent = "Last guess was too high!";
+        }
+  }
+
+    guessCount++;
+    guessField.value = "";
+    guessField.focus();
+}
 
 
